10ff69cdcc7be7b34a33465013133c5dcadfc5eb
Jake Levine <jake@agileramen.com>
2016-10-17 10:07:56 -0700

    Add expanding description and redux actions

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,39 @@
 // Import libraries
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
+
 // Create component
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -25,5 +46,9 @@ const styles = {
   }
 };
 
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
 // Export component
-export default ListItem;
+export default connect(mapStateToProps, actions)(ListItem);
